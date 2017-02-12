2fa32b6ad0ebf8686e5aa8706e6e46cfbed7cb9a
Jonathan Unsworth <jonathan@protonmail.com>
2017-02-12 21:40:59 +0200

    Added ‘AlamofireWorker.swift’ file...

---
+++ AppOneLib/src/Workers/AlamofireWorker.swift
@@ -0,0 +1,19 @@
+//
+//  AlamofireWorker.swift
+//  AppOneLib
+//
+//  Created by Jonathan Unsworth on 2017/02/12.
+//  Copyright © 2017 Jonathan Unsworth. All rights reserved.
+//
+
+import Foundation
+
+class AlamoFireWorker {
+    
+    static let sharedInstance = AlamoFireWorker()
+    
+    func getURLRequestWorker(url:String) -> String {
+        return "Hello World"
+    }
+    
+}
